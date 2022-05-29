3ecbdd98f2e30170aff0083293adebf2efe9b0fa
SohilaArafa <sohilaarafa2001@gmail.com>
2022-05-29 17:44:17 +0200

    activating the buttons of the cards

--- FrontSide/screens/CategoryMenu.js
+++ FrontSide/screens/CategoryMenu.js
@@ -1,33 +1,40 @@
 import { View, Text, SafeAreaView , StyleSheet, ScrollView } from 'react-native'
 import React , {useState , useEffect} from 'react'
 import {Button, Card } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 const CategoryMenu = () => {
     const Data = [
         {
             id : 1,
             name : 'Nigiri' ,
+            screen : "Nigiri" ,
             uri : 'https://thumbor.thedailymeal.com/gUfAylED0IS1nuSLZSRV_Kt9AEc=/870x565/filters:format(webp)/https://www.thedailymeal.com/sites/default/files/recipe/2016/shutterstock_123733927.jpg'
         } , 
         {
             id : 2,
             name : 'Fried Rolls' ,
+            screen : "FriedRolls" ,
             uri : 'https://media.istockphoto.com/photos/hot-tempura-roll-with-prawn-and-cheese-on-black-background-picture-id1144739441?k=20&m=1144739441&s=612x612&w=0&h=IzoCbpoz5WSDMEo6gQl4DdKHTum6SSzT02mAqKcjJtE='
         }, {
              id : 3,
             name : 'Ura Maki Rolls' ,
+            screen : "UraMaki" ,
             uri : 'https://www.reyesgutierrez.com/wp-content/uploads/2020/07/Uramaki-mango-930x620.jpg'
         }, 
         {
             id : 4,
             name : 'Special Ura Maki Rolls' ,
+            screen : "SpecialUra" ,
             uri : 'https://media-cdn.tripadvisor.com/media/photo-s/19/de/fc/f4/uramaki-tuna-special.jpg'
         }, {
             id : 5 ,
             name : 'Drinks' ,
+            screen : "Drinks" ,
             uri : 'https://domf5oio6qrcr.cloudfront.net/medialibrary/8931/Smoothie-post.jpg'
         }
     ]
+    const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.container}>
         <ScrollView>
@@ -38,7 +45,7 @@ const CategoryMenu = () => {
                 <Card.Divider/>
                <Card.Image  style={styles.image} source= {{uri : category.uri}} />
                <Card.Divider/>
-               <Button title="View" style={styles.viewButton}/>
+               <Button title="View" style={styles.viewButton} onPress={() => navigation.navigate(category.screen)}/>
 
               </Card>
             )   )
@@ -71,7 +78,7 @@ export default CategoryMenu
         //backgroundColor : "white" ,
         alignItems: "center",
         backgroundColor: "#DDDDDD",
-        padding: 10
+        padding: 10 
 
       }
   });
